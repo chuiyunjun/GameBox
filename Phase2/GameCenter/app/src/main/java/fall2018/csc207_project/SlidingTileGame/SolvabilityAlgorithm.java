56d4376e46582576b39a13d5eefd672304d53f68
peterzhang <junmingpeter.zhang@mail.utoronto.ca>
2018-11-20 01:03:00 -0500

    delete "public" signature of the class

--- Phase2/GameCenter/app/src/main/java/fall2018/csc207_project/SlidingTileGame/SolvabilityAlgorithm.java
+++ Phase2/GameCenter/app/src/main/java/fall2018/csc207_project/SlidingTileGame/SolvabilityAlgorithm.java
@@ -9,7 +9,7 @@ import java.util.ArrayList;
  * https://www.cs.bham.ac.uk/~mdr/teaching/modules04/java2/TilesSolvability.html
  */
 
-public class SolvabilityAlgorithm {
+class SolvabilityAlgorithm {
 
     /**
      * ID of tiles in the board
