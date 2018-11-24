30cee2019868d5c090b4dc7066206d06da97702b
Yuchen Tong <yuchen.tong@mail.utoronto.ca>
2018-11-24 14:07:30 -0500

    1. Add necessary classes for game2048(temporary empty) 2. Add 2048 game avtivity xml file(warnings unfixed) and necessary colors for each component to colors.xml.

---
+++ Phase2/GameCenter/app/src/main/java/fall2018/csc207_project/game2048/BoardView.java
@@ -0,0 +1,12 @@
+package fall2018.csc207_project.game2048;
+
+import android.content.Context;
+import android.widget.GridLayout;
+
+public class BoardView extends GridLayout {
+
+    public BoardView(Context context) {
+        super(context);
+    }
+
+}
---
+++ Phase2/GameCenter/app/src/main/java/fall2018/csc207_project/game2048/Tile.java
@@ -0,0 +1,4 @@
+package fall2018.csc207_project.game2048;
+
+public class Tile {
+}
