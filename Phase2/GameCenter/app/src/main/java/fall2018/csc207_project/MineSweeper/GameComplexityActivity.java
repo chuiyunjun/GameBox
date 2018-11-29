1b883df61aff6928a2751e3e65953d11d1bcc6e6
peterzhang <junmingpeter.zhang@mail.utoronto.ca>
2018-11-28 19:30:27 -0500

    update new complexity mode

--- Phase2/GameCenter/app/src/main/java/fall2018/csc207_project/MineSweeper/GameComplexityActivity.java
+++ Phase2/GameCenter/app/src/main/java/fall2018/csc207_project/MineSweeper/GameComplexityActivity.java
@@ -1,12 +1,15 @@
 package fall2018.csc207_project.MineSweeper;
 
 import android.app.Activity;
+import android.app.AlertDialog;
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 
 import java.util.LinkedList;
+import java.util.Random;
 
 import fall2018.csc207_project.GameCenter.GlobalCenter;
 import fall2018.csc207_project.GameCenter.LocalGameCenter;
@@ -24,10 +27,13 @@ public class GameComplexityActivity extends Activity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_complexity);
         globalCenter = (GlobalCenter) (getIntent().getSerializableExtra("GlobalCenter"));
-        localCenter = globalCenter.getLocalGameCenter(globalCenter.getCurrentPlayer().getUsername());
+        localCenter = globalCenter.getLocalGameCenter(globalCenter.getCurrentPlayer().
+                getUsername());
         add10BombsButtonListener();
         add15BombsButtonListener();
         add20BombsButtonListener();
+        addCrazyModeButtonListener();
+        addLuckyModeButtonListener();
 
     }
 
@@ -57,6 +63,83 @@ public class GameComplexityActivity extends Activity {
         });
     }
 
+    private void addCrazyModeButtonListener(){
+        Button crazyButton = findViewById(R.id.crazy);
+        crazyButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                AlertDialog.Builder builder1 =
+                        new AlertDialog.Builder(GameComplexityActivity.this);
+                builder1.setMessage("This mode is crazy! The number of bombs is quite " +
+                        "tricky, and this mode is just for fun, " +
+                        "not for mark, would you like to go?");
+                builder1.setCancelable(true);
+
+                builder1.setPositiveButton(
+                        "Yes, please",
+                        new DialogInterface.OnClickListener() {
+                            public void onClick(DialogInterface dialog, int id) {
+                                switchToGame(98);
+                                finish();
+                            }
+                        });
+
+                builder1.setNegativeButton(
+                        "No, thanks",
+                        new DialogInterface.OnClickListener() {
+                            public void onClick(DialogInterface dialog, int id) {
+                                dialog.cancel();
+                            }
+                        });
+
+                AlertDialog alert11 = builder1.create();
+                alert11.show();
+            }
+        });
+    }
+
+    private void addLuckyModeButtonListener(){
+        Button luckButton = findViewById(R.id.lucky);
+        luckButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                AlertDialog.Builder builder1 =
+                        new AlertDialog.Builder(GameComplexityActivity.this);
+                builder1.setMessage("Good luck for you! The number of bombs is random " +
+                        ", and this mode is just for fun, " +
+                        "not for mark, would you like to go?");
+                builder1.setCancelable(true);
+
+                builder1.setPositiveButton(
+                        "Yes, please",
+                        new DialogInterface.OnClickListener() {
+                            public void onClick(DialogInterface dialog, int id) {
+                                Random random = new Random();
+                                int comp = random.nextInt(101);
+                                if (comp > 100) {
+                                    comp = 100;
+                                } else if (comp < 0) {
+                                    comp = 0;
+                                }
+                                switchToGame(comp);
+                                finish();
+                            }
+                        });
+
+                builder1.setNegativeButton(
+                        "No, thanks",
+                        new DialogInterface.OnClickListener() {
+                            public void onClick(DialogInterface dialog, int id) {
+                                dialog.cancel();
+                            }
+                        });
+
+                AlertDialog alert11 = builder1.create();
+                alert11.show();
+            }
+        });
+    }
+
     private void add20BombsButtonListener(){
         Button l20Button = findViewById(R.id.grid20);
         l20Button.setOnClickListener(new View.OnClickListener() {
@@ -80,7 +163,8 @@ public class GameComplexityActivity extends Activity {
     private void switchToGame(int complexity) {
         LinkedList<Object> settings = new LinkedList<>();
         settings.add(complexity);
-        MineSweeperGame game = (MineSweeperGame) localCenter.newGame("minesweeperGame", settings);
+        MineSweeperGame game = (MineSweeperGame) localCenter.newGame("minesweeperGame",
+                settings);
         Intent tmp = new Intent(this, GameActivity.class);
         tmp.putExtra("GlobalCenter", globalCenter);
         localCenter.setCurGame(game);
