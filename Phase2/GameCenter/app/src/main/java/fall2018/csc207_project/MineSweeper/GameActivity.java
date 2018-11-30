6c8f3a642aaae4bb47189e93da6580aebd19027b
peterzhang <junmingpeter.zhang@mail.utoronto.ca>
2018-11-29 19:29:23 -0500

    "delete dead code"

--- Phase2/GameCenter/app/src/main/java/fall2018/csc207_project/MineSweeper/GameActivity.java
+++ Phase2/GameCenter/app/src/main/java/fall2018/csc207_project/MineSweeper/GameActivity.java
@@ -7,7 +7,6 @@ import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
-import android.widget.Toast;
 
 import java.text.SimpleDateFormat;
 import java.util.Calendar;
