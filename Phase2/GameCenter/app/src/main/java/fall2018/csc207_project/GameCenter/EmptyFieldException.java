138faca6f87050b43502bc43611e7bd08e9b7a3b
Yuchen Tong <yuchen.tong@mail.utoronto.ca>
2018-11-30 01:52:43 -0500

    Empty project due to merging issues

--- Phase2/GameCenter/app/src/main/java/fall2018/csc207_project/GameCenter/EmptyFieldException.java
+++
@@ -1,8 +0,0 @@
-package fall2018.csc207_project.GameCenter;
-
-/**
- * Empty account Exception
- */
-
-public class EmptyFieldException extends Exception{
-}
--- Phase2/GameCenter/app/src/main/java/fall2018/csc207_project/GameCenter/PasswordNotMatchingException.java
+++
@@ -1,4 +0,0 @@
-package fall2018.csc207_project.GameCenter;
-
-public class PasswordNotMatchingException extends Exception {
-}
