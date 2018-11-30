ffa01afbc82a25d8a30ecdcd7ca9d7705a2ee36e
daniel_fan <fanyuchensx@gmail.com>
2018-11-30 03:59:11 -0500

    add comments, fix warning and code smells for GameCenter

--- Phase2/GameCenter/app/src/main/java/fall2018/csc207_project/GameCenter/GlobalCenter.java
+++ Phase2/GameCenter/app/src/main/java/fall2018/csc207_project/GameCenter/GlobalCenter.java
@@ -11,24 +11,43 @@ import java.util.HashMap;
 import java.util.Map;
 import java.util.Observable;
 import java.util.Observer;
+/**
+ * The GlobalCenter for Application
+ */
 
 public class GlobalCenter implements Serializable, Observer{
 
+    /**
+     * the serial number of global center
+     */
     private static final long serialVersionUID = 2L;
 
+    /**
+     * the map from String to account
+     * the map from String to LocalGameCenter
+     * the map from String to ScoreBoard
+     * the current account of currentPlayer
+     */
     private Map<String, Account> accounts = new HashMap<>();
     private Map<String, LocalGameCenter> localCenters = new HashMap<>();
     private Map<String, ScoreBoard> scoreboards = new HashMap<>(); // gameName -> ScoreBoard
     private Account currentPlayer;
 
-    public Map getAccounts() {
-        return accounts;
-    }
 
+    /**
+     * get the scoreboard
+     * @return scoreboard
+     */
     public Map getScoreBoards(){
         return scoreboards;
     }
 
+    /**
+     * check the username and password
+     * @param username the username which user input
+     * @param password the password which user input
+     * @return whether the username and password is matched
+     */
     public Boolean signIn(String username, String password){
 
         if (accounts.containsKey(username)){
@@ -43,6 +62,14 @@ public class GlobalCenter implements Serializable, Observer{
         return false;
     }
 
+    /**
+     * sign up a nwe user
+     * @param username the username which user input
+     * @param password1 the password which user input
+     * @param password2 the password which user confirmed
+     * @return Sign in the game use the new account
+     * @throws Exception
+     */
     public boolean signUp(String username, String password1, String password2) throws Exception{
         if (username.equals("") || password1.equals("") || password2.equals(""))
             throw new EmptyFieldException();
@@ -60,29 +87,47 @@ public class GlobalCenter implements Serializable, Observer{
         }
     }
 
+    /**
+     * add the local game center for each user
+     * @param username the username which the user log in
+     */
     private void addLocalGameCenter(String username) {
         LocalGameCenter newLocalCenter = new LocalGameCenter();
         localCenters.put(username, newLocalCenter);
     }
 
+    /**
+     * get the information of current player
+     * @return current player
+     */
     public Account getCurrentPlayer(){
         return currentPlayer;
     }
 
-    public void signOut(){
-        currentPlayer = null;
-    }
-
+    /**
+     * get the local game center for each user
+     * @param username the user which user log in
+     * @return local game center
+     */
     public LocalGameCenter getLocalGameCenter(String username) {
         return localCenters.get(username);
     }
 
+    /**
+     * add game to scoreboard
+     * @param gameName the name of game which will be added to scoreboard
+     * @param scoreboard the scoreboard that the score will be added to
+     */
     public void addScoreBoard(String gameName, ScoreBoard scoreboard){
         if (!scoreboards.containsKey(gameName)){
             scoreboards.put(gameName, scoreboard);
         }
     }
 
+    /**
+     * save all the data of this application
+     * @param context the global game center
+     */
     public void saveAll(Context context){
         ObjectOutputStream out = null;
         try {
