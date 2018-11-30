c89cbb1de08653efe6e2f01991e410692d3b768d
daniel_fan <fanyuchensx@gmail.com>
2018-11-30 04:24:34 -0500

    add comments and finally complete the interface of this application

--- Phase2/GameCenter/app/src/main/java/fall2018/csc207_project/Interfaces/GameActivityInterface.java
+++ Phase2/GameCenter/app/src/main/java/fall2018/csc207_project/Interfaces/GameActivityInterface.java
@@ -1,5 +1,11 @@
 package fall2018.csc207_project.Interfaces;
 
+/**
+ * interface of Game Activity Interface
+ */
 public interface GameActivityInterface {
+    /**
+     * switch to ScoreBoard
+     */
     void switchToScoreBoard();
 }
